fff83b339e846d19370d75530d167b0162e8a768
Janita M <48458664+JanitaM@users.noreply.github.com>
2020-01-01 10:30:10 -0500

    created Adobe XD prototype and used it to code my HTML and CSS files

---
+++ main.js
@@ -0,0 +1,17 @@
+// Default state
+let state = {
+  cVal: "",
+  fVal: ""
+}
+
+let button = document.getElementById("button");
+let cInput = document.getElementById('c-input').innerHTML;
+let fInput = document.getElementById('f-input').innerHTML;
+
+button.addEventListener("click", () => {
+
+  console.log('clicked');
+
+
+
+});
