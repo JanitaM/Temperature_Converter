3abf3fdc0d969c98d3b2286a2e7f38cfcddce7b4
Janita M <48458664+JanitaM@users.noreply.github.com>
2020-01-01 13:35:19 -0500

    change submit button to reset button when clicked

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ let state = {
 }
 
 let button = document.getElementById("button");
-let cInput = document.getElementById('c-input').innerHTML;
-let fInput = document.getElementById('f-input').innerHTML;
+let cInput = document.getElementById('c-input');
+let fInput = document.getElementById('f-input');
 
 button.addEventListener("click", () => {
 
+  button.className = 'reset-btn';
+  button.innerHTML = "RESET";
   console.log('clicked');
-
-
-
 });
